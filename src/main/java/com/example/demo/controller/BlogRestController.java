9f15e88a9e1c38ff118663b202d7d7aa340b0d2b
admin <saesaelee2@gmail.com>
2025-11-27 01:33:43 +0900

    6주차 블로그 게시판 2

--- src/main/java/com/example/demo/controller/BlogRestController.java
+++ src/main/java/com/example/demo/controller/BlogRestController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
 import com.example.demo.model.domain.Article;
 import com.example.demo.model.service.AddArticleRequest;
 import com.example.demo.model.service.BlogService;
@@ -7,21 +8,24 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 @RequiredArgsConstructor
-@RestController // @Controller + @ResponseBody
+
+@Controller @ResponseBody
 public class BlogRestController {
     private final BlogService blogService;
     @PostMapping("/api/articles") // post 요청
+    @ResponseBody
     public ResponseEntity<Article> addArticle(@ModelAttribute AddArticleRequest request) { // 아직 없음(에러)
     Article saveArticle = blogService.save(request); // 게시글 저장
     return ResponseEntity.status(HttpStatus.CREATED) // 상태 코드 및 게시글 정보 반환
         .body(saveArticle);
     }
-}
-
 
     @GetMapping("/favicon.ico")
+    @ResponseBody
     public void favicon() {
         // 아무 작업도 하지 않음
     }
+}
 
--- src/main/java/com/example/demo/model/service/BlogService.java
+++ src/main/java/com/example/demo/model/service/BlogService.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import com.example.demo.model.domain.Article;
 import com.example.demo.model.repository.BlogRepository;
@@ -32,6 +35,24 @@ public class BlogService {
 		// .build();
 		return blogRepository.save(request.toEntity());
 	}
+	public Optional<Article> findById(Long id) { // 게시판 특정 글 조회
+	return blogRepository.findById(id);
+	}
+	public void update(Long id, AddArticleRequest request) {
+	Optional<Article> optionalArticle = blogRepository.findById(id); // 단일 글 조회
+	optionalArticle.ifPresent(article -> { // 값이 있으면
+	article.update(request.getTitle(), request.getContent()); // 값을 수정
+	blogRepository.save(article); // Article 객체에 저장
+	});
+	}
+	
+	@PutMapping("/api/article_edit/{id}")
+	public String updateArticle(@PathVariable Long id, @ModelAttribute AddArticleRequest request) {
+	blogService.update(id, request);
+	return "redirect:/article_list"; // 글 수정 이후 .html 연결
+	}
+
+
 
 	public void update(Long id, AddArticleRequest request) {
 		Article article = blogRepository.findById(id)
